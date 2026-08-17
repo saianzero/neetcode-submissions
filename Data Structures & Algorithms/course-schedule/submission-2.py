bf1d2827df0e74b6f8af5c7b3eb7639171e48157
Sai Ankith <65500748+saianzero@users.noreply.github.com>
2026-08-17 16:28:31 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = set()
+        in_recur = set()
+
+        adj = defaultdict(list)
+
+        if not prerequisites:
+            return True
+
+        for u,v in prerequisites:
+            adj[v].append(u)
+
+        def dfs(u):
+            visited.add(u)
+            in_recur.add(u)
+
+            for v in adj[u]:
+                if v in visited and v in in_recur:
+                    return False
+
+                if v not in visited:
+                    if not dfs(v):
+                        return False
+            in_recur.remove(u)
+            return True
+        
+        for u in range(numCourses):
+            if u not in visited:
+                if not dfs(u):
+                    return False
+        return True
+        
